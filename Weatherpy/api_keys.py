ffa8073c5fd80bee00a1b8895e8c2f2a32765f69
Monalipatel <monalipatel19@gmail.com>
2020-04-10 18:07:33 -0400

    Update api_keys.py

--- Weatherpy/api_keys.py
+++ Weatherpy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "810840a2a3e2da79030ed91b203eeff8"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyCTkODhsUjXWplva9VPp0_aXICCeP0u_vI"
+g_key = ""
