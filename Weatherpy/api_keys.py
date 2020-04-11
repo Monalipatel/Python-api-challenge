679c57b0f0ff9ae5fa115622a3d9a5cf07b78cff
Monali Patel <monalipatel@Monalis-MacBook-Air.local>
2020-04-11 09:32:48 -0400

    add files

--- Weatherpy/api_keys.py
+++ Weatherpy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = ""
+weather_api_key = "810840a2a3e2da79030ed91b203eeff8"
 
 # Google API Key
-g_key = ""
+g_key = "AIzaSyCTkODhsUjXWplva9VPp0_aXICCeP0u_vI"
